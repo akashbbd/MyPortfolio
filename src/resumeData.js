4b500049d886cbea6132c1efcdba613a43ce2db4
akashbbd <akash.inps2@gmail.com>
2021-08-14 19:24:57 +0530

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,133 @@
+let resumeData = {
+    // "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Akash Kumar",
+    "role": "Fullstack Developer",
+    "linkedinId":"akash-kumar-57339a1ba",
+    // "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/akash-kumar-57339a1ba/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/akashbbd",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/rbhatia46",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"I am currently a pre-final year student at Babu Banarasi Das Northern India Institute of Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"Babu Banarasi Das Northern India Institute of Technology",
+        "specialization":"Information Technology",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2022",
+        "Percentage":"GPA: 7.56"
+      },
+      {
+        "UniversityName":"Green View Public School",
+        "specialization":"XII",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2018",
+        "Percentage":"Percentage: 67.2"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"____",
+    "skills":[
+      {
+        "skillname":"Java"
+      },
+      {
+        "skillname":"C"
+      },
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"PHP"
+      },
+      {
+        "skillname":"SQL"
+      },
+      {
+        "skillname":"HTML"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"ReactJS"
+      },
+      {
+        "skillname":"NodeJS"
+      },
+      {
+        "skillname":"ExpressJS"
+      },
+      {
+        "skillname":"MongoDB"
+      },
+      {
+        "skillname":"Firebase"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    // "testimonials":[
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   },
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   }
+    // ]
+  }
+  
+  export default resumeData
